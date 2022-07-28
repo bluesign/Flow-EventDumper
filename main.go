384527e1455fbf085e681c690b75b6643e703f0a
root <deniz@edincik.com>
2022-07-28 18:27:41 +0000

    some fixes

--- main.go
+++ main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-	"database/sql"
 	"context"
+	"database/sql"
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"github.com/Jeffail/gabs"
+	badger "github.com/dgraph-io/badger/v2"
+	"github.com/dgraph-io/badger/v2/pb"
 	"log"
 	"os"
 	"strconv"
 	"strings"
-"time"
-	"github.com/Jeffail/gabs"
-	badger "github.com/dgraph-io/badger/v2"
-	 "github.com/dgraph-io/badger/v2/pb"
+	"time"
 
 	_ "github.com/mattn/go-sqlite3"
 	flow "github.com/onflow/flow-go/model/flow"
@@ -22,45 +22,46 @@ import (
 )
 
 var (
-	dbMap map[string]*sql.DB = nil
+	dbMap map[string]*EventWriter = nil
 )
 
-type Envelope struct {
-	Type  string      `json:type`
-	Value interface{} `json:value`
+type EventWriter struct {
+	db           *sql.DB
+	eventName    string
+	writeChannel chan []any
 }
 
-func getInnerField(v map[string]interface{}, key string) string {
-	value := v[key]
+func (ew *EventWriter) Run() {
 
-	switch vv := value.(type) {
+	for {
+		data, ok := <-ew.writeChannel
+		if ok == false {
+			fmt.Println("Channel Close ", ok)
+			break
+		}
 
-	case map[string]interface{}:
-		return getInnerField(vv, key)
+		_, err := ew.db.Exec(string(data[0].(string)), data[1:]...)
+		if err != nil {
 
-	case string:
-		return vv
+			if ew.eventName != "A.4eb8a10cb9f87357.NFTStorefront.ListingCompleted" {
 
-	default:
-		fmt.Println(vv)
+				fmt.Println(ew.eventName)
+				fmt.Println(data[0])
+				fmt.Println(data)
+				log.Fatal(err)
+			}
+		}
 	}
 
-	return "DNZ"
 }
 
-func ensureDb(contractEvent string, samplePayload string) *sql.DB {
+func NewEventWriter(eventName string, payload string) *EventWriter {
 
-	v, ok := dbMap[contractEvent]
-	if ok {
-		return v
-	}
-
-	parts := strings.Split(contractEvent[2:], ".")
+	parts := strings.Split(eventName[2:], ".")
 	path := "db/" + strings.Join(parts[:len(parts)-1], "/")
 
 	fullPath := path + "/" + parts[len(parts)-1] + ".db"
 
-	//lets make target sqlite
 	os.MkdirAll(path, 0777)
 	os.Remove(fullPath)
 
@@ -69,7 +70,7 @@ func ensureDb(contractEvent string, samplePayload string) *sql.DB {
 		log.Fatal(err)
 	}
 
-	jsonParsed, err := gabs.ParseJSON([]byte(samplePayload))
+	jsonParsed, err := gabs.ParseJSON([]byte(payload))
 	if err != nil {
 		panic(err)
 	}
@@ -83,9 +84,9 @@ func ensureDb(contractEvent string, samplePayload string) *sql.DB {
 		fieldName := value["name"].(string)
 		fieldType := xvalue["type"].(string)
 		if fieldType == "Optional" {
-			if xvalue["value"]==nil{
-				fieldType="text"
-			}else{
+			if xvalue["value"] == nil {
+				fieldType = "text"
+			} else {
 				fieldType = xvalue["value"].(map[string]interface{})["type"].(string)
 			}
 		}
@@ -99,7 +100,6 @@ func ensureDb(contractEvent string, samplePayload string) *sql.DB {
 	}
 
 	fields = fmt.Sprintf("%s note text", fields)
-	//crrate events table
 	sqlStmt := fmt.Sprintf(`
 	create table Events (id integer not null primary key, blockID text, transactionID text, transactionIndex integer, eventIndex integer, eventType text, %s);
 	`, fields)
@@ -109,22 +109,37 @@ func ensureDb(contractEvent string, samplePayload string) *sql.DB {
 		log.Printf("%q: %s\n", err, sqlStmt)
 		return nil
 	}
-
-
 	sdb.Exec(`PRAGMA journal_mode = OFF;
 	PRAGMA synchronous = 0;
 	PRAGMA cache_size = 1000000;
 	PRAGMA locking_mode = EXCLUSIVE;
 	PRAGMA temp_store = MEMORY;`)
 
-	dbMap[contractEvent] = sdb
-	return sdb
+	return &EventWriter{
+		db:           sdb,
+		eventName:    eventName,
+		writeChannel: make(chan []any, 10),
+	}
+
+}
+
+func ensureDb(contractEvent string, samplePayload string) *EventWriter {
+
+	v, ok := dbMap[contractEvent]
+	if ok {
+		return v
+	}
+
+	writer := NewEventWriter(contractEvent, samplePayload)
+	dbMap[contractEvent] = writer
+	go writer.Run()
+	return writer
 
 }
 
 func main() {
 
-	dbMap = make(map[string]*sql.DB)
+	dbMap = make(map[string]*EventWriter)
 
 	//open badger database
 	db, err := badger.Open(badger.DefaultOptions("/mnt/flow/mainnet16/protocol"))
@@ -133,34 +148,28 @@ func main() {
 	}
 	defer db.Close()
 
+	stream := db.NewStream()
+	// db.NewStreamAt(readTs) for managed mode.
 
-stream := db.NewStream()
-// db.NewStreamAt(readTs) for managed mode.
+	// -- Optional settings
+	stream.NumGo = 32                     // Set number of goroutines to use for iteration.
+	stream.Prefix = []byte{0x66}          //events
+	stream.LogPrefix = "Badger.Streaming" // For identifying stream logs. Outputs to Logger.
+	stream.KeyToList = nil
 
-// -- Optional settings
-stream.NumGo = 16                     // Set number of goroutines to use for iteration.
-stream.Prefix = []byte{0x66} //events 
-stream.LogPrefix = "Badger.Streaming" // For identifying stream logs. Outputs to Logger.
-stream.KeyToList = nil
+	count := 0
+	stream.Send = func(list *pb.KVList) error {
+		for _, kv := range list.GetKv() {
+			count = count + 1
+			if count%10000 == 0 {
+				fmt.Println(count)
+			}
+			//k key
 
+			k := kv.GetKey()
+			v := kv.GetValue()
 
-count:=0
-stream.Send = func(list *pb.KVList) error {
-  	for _,kv :=range(list.GetKv()){
-		count = count + 1
-
-		if count%10000==0{
-			fmt.Println(count)
-		}
-	//k key
-
-		k:=kv.GetKey()
-		v:=kv.GetValue()
-
-
-
-
-		blockID := hex.EncodeToString(k[1:33])
+			blockID := hex.EncodeToString(k[1:33])
 			transactionID := hex.EncodeToString(k[33:65])
 			transactionIndex := hex.EncodeToString(k[65:69])
 			transactionIndexInt, err := strconv.ParseInt(transactionIndex, 16, 32)
@@ -168,108 +177,85 @@ stream.Send = func(list *pb.KVList) error {
 			eventIndex := hex.EncodeToString(k[69:73])
 			eventIndexInt, err := strconv.ParseInt(eventIndex, 16, 32)
 
-
-		//v value
-		var event flow.Event
-				err = msgpack.Unmarshal(v, &event)
-				if err != nil {
-					return fmt.Errorf("could not decode the event: %w", err)
-				}
+			//v value
+			var event flow.Event
+			err = msgpack.Unmarshal(v, &event)
+			if err != nil {
+				return fmt.Errorf("could not decode the event: %w", err)
+			}
 			//fmt.Println(string(event.Payload))
 
-				sdb := ensureDb(string(event.Type), string(event.Payload))
+			writer := ensureDb(string(event.Type), string(event.Payload))
 
-				jsonParsed, err := gabs.ParseJSON(event.Payload)
-				if err != nil {
-					panic(err)
-				}
-				s, _ := jsonParsed.Search("value", "fields").Children()
+			jsonParsed, err := gabs.ParseJSON(event.Payload)
+			if err != nil {
+				panic(err)
+			}
+			s, _ := jsonParsed.Search("value", "fields").Children()
 
-				fields := ""
-				values := ""
-				valuesMarker := ""
-				var vs []any = make([]any,0)
+			var vs []any = make([]any, 0)
+			insertFields := ""
+			valuesMarker := ""
+			for _, child := range s {
+				value := child.Data().(map[string]interface{})
+				xvalue := value["value"].(map[string]interface{})
+				fieldType := xvalue["type"].(string)
+				fieldValue := ""
+				fieldName := value["name"].(string)
 
-				for _, child := range s {
-					value := child.Data().(map[string]interface{})
-					xvalue := value["value"].(map[string]interface{})
-					fieldName := value["name"].(string)
-					fieldType := xvalue["type"].(string)
-					fieldValue := ""
-					if fieldType == "Optional" {
-						if xvalue["value"] != nil {
-							fieldType = xvalue["value"].(map[string]interface{})["type"].(string)
-							fieldValue = fmt.Sprintf("%s",xvalue["value"].(map[string]interface{})["value"])
-						} else {
-							fieldValue = "null"
-						}
+				if fieldType == "Optional" {
+					if xvalue["value"] != nil {
+						fieldType = xvalue["value"].(map[string]interface{})["type"].(string)
+						fieldValue = fmt.Sprintf("%s", xvalue["value"].(map[string]interface{})["value"])
 					} else {
-						switch tt := xvalue["value"].(type) {
-						case string:
-							fieldValue = tt
-						default:
-							b, _ := json.Marshal(xvalue["value"])
-							fieldValue = string(b)
-						}
+						fieldValue = "null"
 					}
-
-					if strings.Contains(fieldType, "Int"){
-						fieldType = "integer"
-						number, _ := strconv.ParseInt(fieldValue, 10, 0)
-						vs = append(vs, number)
-					}else if strings.Contains(fieldType, "Fix") {
-						fieldType = "integer"
-						number, _ := strconv.ParseInt(strings.Replace(fieldValue, ".","",1),10,0)
-						vs = append(vs, number)
-					} else {
-						fieldType = "text"
-						vs = append(vs, fieldValue)
+				} else {
+					switch tt := xvalue["value"].(type) {
+					case string:
+						fieldValue = tt
+					default:
+						b, _ := json.Marshal(xvalue["value"])
+						fieldValue = string(b)
 					}
-					fields = fmt.Sprintf("%s _%s, ", fields, fieldName)
-					valuesMarker = fmt.Sprintf("%s ?,", valuesMarker)
-					values = fmt.Sprintf("%s '%s', ", values, fieldValue)
 				}
 
-				fields = fmt.Sprintf("%s note", fields)
-				values = fmt.Sprintf("%s ''", values)
-				valuesMarker = fmt.Sprintf("%s ?", valuesMarker)
-
-				stmt := fmt.Sprintf(`insert into Events(blockID, transactionID, transactionIndex, eventIndex, eventType, %s) values(?, ?, ?, ?, ?, %s)`, fields, valuesMarker)
-
-				var args []any = make([]any,0)
-				args = append(args, blockID)
-				args = append(args, transactionID)
-				args = append(args, transactionIndexInt)
-				args = append(args, eventIndexInt)
-				args = append(args, event.Type)
-				for _, item := range(vs){
-					args = append(args, item)
+				if strings.Contains(fieldType, "Int") {
+					number, _ := strconv.ParseInt(fieldValue, 10, 0)
+					vs = append(vs, number)
+				} else if strings.Contains(fieldType, "Fix") {
+					number, _ := strconv.ParseInt(strings.Replace(fieldValue, ".", "", 1), 10, 0)
+					vs = append(vs, number)
+				} else {
+					vs = append(vs, fieldValue)
 				}
-				args = append(args, "")
-					
-				//fmt.Println(count, args)
+				insertFields = fmt.Sprintf("%s _%s,", insertFields, fieldName)
+				valuesMarker = fmt.Sprintf("%s ?,", valuesMarker)
+			}
+			stmt := fmt.Sprintf(`insert into Events(blockID, transactionID, transactionIndex, eventIndex, eventType, %s note) values(?, ?, ?, ?, ?, %s ?)`, insertFields, valuesMarker)
 
-				_, err = sdb.Exec(stmt,args...)
-				//blockID, transactionID, transactionIndexInt, eventIndexInt, event.Type, string(event.Payload), vs...)
-				if err != nil {
-					log.Fatal(err)
-				}
+			var args []any = make([]any, 0)
+			args = append(args, stmt)
+			args = append(args, blockID)
+			args = append(args, transactionID)
+			args = append(args, transactionIndexInt)
+			args = append(args, eventIndexInt)
+			args = append(args, event.Type)
+			args = append(args, vs...)
+			args = append(args, "")
+			writer.writeChannel <- args
 
-	
+		}
+		return nil
+	}
 
+	if err := stream.Orchestrate(context.Background()); err != nil {
+		fmt.Println(err)
+	}
 
-	}	
-	return nil 
-}
+	for {
 
-if err := stream.Orchestrate(context.Background()); err != nil {
-  fmt.Println(err)
-}
-
-for{
-
-	time.Sleep(time.Second)
-}
-
+		time.Sleep(time.Second)
+	}
 
 }
